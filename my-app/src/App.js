bd24176f6aeaac16696ca40d72bc5b6a5dd4a25a
Anatoly <iamgerman1711@gmail.com>
2022-08-09 15:29:06 +0300

    my_commit

---
+++ my-app/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap'
+import './App.css';
+
+class Form extends Component {
+
+    state = {
+        advOpen: false
+    }
+
+    handleClick = () => {
+        this.setState(({advOpen}) => ({
+            advOpen: !advOpen
+        }))
+    }
+
+    componentDidMount() {
+        setTimeout(this.handleClick, 3000)
+    }
+
+    render() {
+        return (
+            <Container>
+                <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto"
+                style={{'overflow': 'hidden',
+                        'position': 'relative'}}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input 
+                        ref={this.setInputRef}
+                        type="email"
+                        className="form-control" 
+                        id="exampleFormControlInput1" 
+                        placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea onClick={this.focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    {
+                        this.state.advOpen ? 
+                            <Portal>
+                                <Message />
+                            </Portal> : null
+                    }
+                    
+                </form>
+            </Container>
+        )
+    }
+}
+
+const Portal = (props) => {
+    const node = document.createElement('div');
+    document.body.appendChild(node);
+
+    return ReactDOM.createPortal(props.children, node);
+    
+}
+
+const Message = () => {
+    return(
+        <div
+            style={{'width': '500px', 
+                'height': '150px', 
+                'backgroundColor': 'red', 
+                'position': 'absolute', 
+                'bottom': '0', 
+                'right': '0'}}>
+            Hello
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <Form />
+    );
+}
+
+export default App;
